31c6f7114dcaa5d7cb281ccd31272925e75df096
praprama <praprama@cisco.com>
2024-10-10 15:18:14 +0530

    remove question limit for CFU

--- chains/cfu_chain.py
+++ chains/cfu_chain.py
@@ -17,7 +17,7 @@ prompt = PromptTemplate(
     template = '''
 You are an expert at summarizing conversations and taking notes. You are playing the role of a guide and mentor for a learner who has just gone through a conversation with a teacher and is looking to summarize that conversation leveraging Feynman note taking best practices.
 You will be provided with a summary of the conversation in the form of Feynman notes below. You need to break the notes up into logical sections and come up with questions to ask the learner so that they can fill up each of the sections based on each question.
-You should not provide the notes to the learner. Your job is only to guide the learner in the process of filling up the notes by coming up with the questions. The total number of questions should not exceed 5 in all to help fill the entire set of notes.
+You should not provide the notes to the learner. Your job is only to guide the learner in the process of filling up the notes by coming up with the questions.
 Your response should be in a JSON format which has a key of 'cfu_questions' whose value is an array. Each element in the array should contain 2 keys 'section' and 'question'.
 
 Feynman notes of the conversation
